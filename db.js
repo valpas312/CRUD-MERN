654f58e6fe845031e2abb936fbf2f9f8faea878c
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-02-13 23:08:58 -0300

    Conectando base de datos

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const main = async () => {
+    try{
+        mongoose.connect("mongodb+srv://valpas312:41667229Vv@cluster0.naob6w4.mongodb.net/?retryWrites=true&w=majority")
+        console.log("Connected to database")                
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main();
+
+module.exports = mongoose;
